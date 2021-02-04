f443d9d3a0620955738a507c2bb17e2e849e2d56
Eddie <jhih.ching@gmail.com>
2021-02-05 02:54:00 +0800

    init

---
+++ examples/cc2650lp/simple_central/ccs/config/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2021-02-05 00:21:31.269000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x200041a0
+--define=ICALL_STACK0_START=0xb000
+--define=ICALL_STACK0_ADDR=0xb001
+
